2d61bdf2a2204598323ecaba1606561308fba926
Alex Kiura <kiuraalex@gmail.com>
2016-12-27 09:56:21 +0300

    feat: use in keyword to iterate over iterables

--- chapter1/for_loops.py
+++ chapter1/for_loops.py
@@ -1,4 +1,6 @@
 """Idiomatic ways of writing for loops."""
+
+# Scenario 1: having an index variable in a for loop
 # harmful
 my_container = ['Larry', 'Moe', 'Curly']
 index = 0
@@ -10,3 +12,17 @@ for element in container:
 my_container = ['Larry', 'Moe', 'Curly']
 for index, element in enumerate(my_container):
     print('{}{}'.format(index, element))
+
+
+# Scenario 2: iterating over an iterable using the in keyword
+# harmful
+my_list = ['Larry', 'Moe', 'Curly']
+index = 0
+while index < len(my_list):
+    print(my_list[index])
+    index += 1
+
+# Idiomatic
+my_list = ['Larry', 'Moe', 'Curly']
+for element in my_list:
+    print(element)

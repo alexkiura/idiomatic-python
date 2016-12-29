ac738e1254daed294f57dc1f8ac7e057491383c3
Alex Kiura <kiuraalex@gmail.com>
2016-12-29 23:37:13 +0300

    feat: using str.join to create string from lists

--- chapter2/strings.py
+++ chapter2/strings.py
@@ -1,5 +1,6 @@
 """Idiomatic ways of dealing with strings."""
 
+# Scenario 1: chaining functions to make transformations clear
 # harmful
 book_info = ' The Three Musketeers: Alexandre Dumas'
 formatted_book_info = book_info.strip()
@@ -9,3 +10,14 @@ formatted_book_info = formatted_book_info.replace(':', ' by')
 # Idiomatic
 book_info = ' The Three Musketeers: Alexandre Dumas'
 formatted_book_info = book_info.strip().upper().replace(':', ' by')
+
+# Scenario 2: using ''.join when creating a single string for list elements
+# harmful
+result_list = ['True', 'False', 'File not found']
+result_string = ''
+for result in result_list:
+    result_string += result
+
+# Idiomatic
+result_list = ['True', 'False', 'File not found']
+result_string = ''.join(result_list)

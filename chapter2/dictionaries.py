eb9f95353e8722ce5b3381e2e825660c4aa2a005
Alex Kiura <kiuraalex@gmail.com>
2017-01-02 04:51:36 +0300

    feat: using dict.get to provide default values when querying a dict

--- chapter2/dictionaries.py
+++ chapter2/dictionaries.py
@@ -18,3 +18,15 @@ def apply_operation(left_operand, right_operand, operator):
     import operator as op
     operator_mapper = {'+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv}
     return operator_mapper[operator](left_operand, right_operand)
+
+# Scenario 2: using dict.get to provide default values
+# harmful
+log_severity = None
+if 'severity' in configuration:
+    log_severity = configuration['severity']
+else:
+    log_severity = 'Info'
+
+
+# Idiomatic
+log_severity = configuration.get('severity', 'Info')

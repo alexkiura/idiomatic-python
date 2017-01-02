55278b7d8168fcb930aad71231ff6966972a7264
Alex Kiura <kiuraalex@gmail.com>
2017-01-02 04:10:25 +0300

    feat: using * to represent the 'rest' of a list

--- chapter2/lists.py
+++ chapter2/lists.py
@@ -62,3 +62,22 @@ def contains_zero(iterable):
 def contains_zero(iterable):
     # 0 is falsy so all(iterable) works?
     return not all(iterable)
+
+# Scenario 6: using * to represent the rest of a list
+# harmful
+some_list = ['a', 'b', 'c', 'd', 'e']
+first, second, rest = some_list[0], some_list[1], some_list[2:]
+print(rest)
+first, middle, last = some_list[0], some_list[1: -1], some_list[-1]
+print(middle)
+head, penultimate, last = some_list[:-2], some_list[-2], some_list[-1]
+print(head)
+
+# Idiomatic
+some_list = ['a', 'b', 'c', 'd', 'e']
+first, second, *rest = some_list
+print(rest)
+first, *middle, last = some_list
+print(middle)
+*head, penultimate, last = some_list
+head

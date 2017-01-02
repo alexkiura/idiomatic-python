8dee03ffbaab1884af29b7fb1e623634ac3db0d7
Alex Kiura <kiuraalex@gmail.com>
2017-01-02 04:42:59 +0300

    feat: using dict as switch..case

---
+++ chapter2/dictionaries.py
@@ -0,0 +1,20 @@
+"""Idiomatic ways of dealing with lists."""
+
+# Scenario 1: using dict as a substitute for a switch ...case statement
+# harmful
+def apply_operation(left_operand, right_operand, operator):
+    if operator == '+':
+        return left_operand + right_operand
+    elif operator == '-':
+        return left_operand - right_operand
+    elif operator == '*':
+        return left_operand * right_operand
+    elif operator == '/':
+        return left_operand / right_operand
+
+
+# Idiomatic
+def apply_operation(left_operand, right_operand, operator):
+    import operator as op
+    operator_mapper = {'+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv}
+    return operator_mapper[operator](left_operand, right_operand)

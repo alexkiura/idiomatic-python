569231cbda7e9f4e7029f13ff44e95a095518e9c
Alex Kiura <kiuraalex@gmail.com>
2017-01-02 03:28:35 +0300

    feat: using negative indices

--- chapter2/lists.py
+++ chapter2/lists.py
@@ -11,3 +11,17 @@ for element in some_other_list:
 # Idiomatic
 some_other_list = range(10)
 some_list = [element + 5 for element in some_other_list if is_prime(element)]
+
+# Scenario 2: making use of negative indices
+# harmful
+def get_suffix(word):
+    word_length = len(word)
+    return word[word_length - 2:]
+# shorter harmful
+def get_suffix(word):
+    word_length = len(word)
+    return [len(word) - 2:]
+
+# Idiomatic
+def get_suffix(word):
+    return word[-2:]

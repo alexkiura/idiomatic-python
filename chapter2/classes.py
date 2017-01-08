dc3ee5a9c2fb1f8dc5e4cc0d5a043ed2525503ba
Alex Kiura <kiuraalex@gmail.com>
2017-01-08 23:50:33 +0300

    feat: using self or @classmethod when referencing class arributes

---
+++ chapter2/classes.py
@@ -0,0 +1,36 @@
+"""Idiomatic ways of writing classes."""
+
+# Scenarion 1: using self or @classmethod when referring to class attributes
+
+# harmful
+class Blog():
+    __tablename = 'Blog'
+
+    def table_name(self):
+        return Blog.__tablename__
+
+
+class DerivedBlog(Blog):
+    __tablename__ = 'derived blog'
+
+b = DerivedBlog()
+print(b.table_name())  # prints blog
+
+
+# Idiomatic
+class Blog():
+    __tablename__ = 'blog'
+
+    def table_name(self):
+        return self.__tablename__
+
+    @classmethod
+    def other_table_name(cls):
+        return cls.__tablename__
+
+
+class DerivedBlog():
+    __tablename__ = 'derived blog'
+
+b = DerivedBlog()
+print(b.tablename())  # prints derived blog

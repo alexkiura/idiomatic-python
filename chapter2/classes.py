a2fefc89ce97a66ff9e7a12153ff9aa2b464e96d
Alex Kiura <kiuraalex@gmail.com>
2017-01-12 00:41:42 +0300

    feat: using leading underscores to denote private data

--- chapter2/classes.py
+++ chapter2/classes.py
@@ -63,3 +63,70 @@ def get_size(some_object):
 
     elif isinstance(some_object, (int, float)):
         return int(some_object)
+
+# Scenario 3: using leading underscores to denote private data:
+# harmful
+class Foo():
+    def __init__(self):
+        self.id = 8
+        self.value = self.get_value()
+
+    def get_value(self):
+        pass
+
+    def should_destroy_earth(self):
+        return self.id == 42
+
+
+class Baz(Foo):
+    def get_value(self, some_new_parameter):
+        """Creating a Baz instance fails because get_value is defined
+        in the Base class __init__ method and the Base class definition takes
+        no parameter.
+        ."""
+        pass
+
+
+class Qux(Foo):
+    """We are unaware of Foo's internals.
+    We unknowingly create an instance attribute named id and set it to 42.
+    This destroys the earth."""
+
+    def __init__(self):
+        super(Qux, self).__init__()
+        self.id = 42
+
+q = Qux()
+b = Baz() # raises TypeError
+q.should_destroy_earth() # returns True
+q.id == 42 # returns True
+
+# Idiomatic
+class Foo():
+    def __init__(self):
+        """Since id is important, we make it a private variable"""
+        self.__id = 8
+        self.value = self.__get_value()
+
+    def get_value(self):
+        pass
+
+    def should_destroy_earth(self):
+        return self.__id == 42
+
+    __get_value = get_value
+
+
+class Baz(Foo):
+    def get_value(self, some_new_parameter):
+        pass
+
+class Qux(Foo):
+    def __init__(self):
+        self.id = 42
+        super(Qux, self).__init__()
+
+q = Qux()
+b = Baz() # does not raise TypeError
+q.should_destroy_earth()
+q.id == 42

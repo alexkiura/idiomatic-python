8163a1c911e1a76a3f30e2b5fbfb4a35953f0f30
Alex Kiura <kiuraalex@gmail.com>
2017-01-19 18:51:13 +0300

    feat: using if __name__ == __main__
    checkinf if __name__ == __main__.py to allow both importing and executing of code

--- chapter3/imports.py
+++ chapter3/imports.py
@@ -90,3 +90,35 @@ from gizmo.client.contrib.utils import GizmoHelper
 
 # Client code
 from gizmo import Gizmo, GizmoHelper
+
+# Scenario 6: using if__name__ == __main to allow both importing and execution
+# Harmful
+import os
+import sys
+
+FIRST_NUMBER = float(sys.argv[1])
+SECOND_NUMBER = float(sys.argv[2])
+
+def divide(a, b):
+    return a / b
+
+
+# Every time this module is imported the code below is executed
+if SECOND_NUMBER != 0:
+    print(divide(FIRST_NUMBER, SECOND_NUMBER))
+
+
+# Idiomatic
+import os
+import sys
+
+
+def divide(a, b):
+    return a / b
+
+if __name__ == '__main__':
+    first_number = sys.argv[1]
+    second_number = sys.argv[2]
+
+    if second_number != 0:
+        print(divide(first_number, second_number))
